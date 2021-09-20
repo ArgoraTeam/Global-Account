9b5126454462f1d7f125dd6e697517099edf768e
bidetaggle <bidetaggle@gmail.com>
2021-09-20 15:52:34 +0200

    ⚒️ adapt to getGlobalStorageOfWallet new specs

--- src/components/Wallet/index.tsx
+++ src/components/Wallet/index.tsx
@@ -9,7 +9,8 @@ import { ctx } from '../../utils';
 function Wallet() {
   const {addr} = useParams<PathParams>();
   const {walletAddr} = useContext(ctx);
-  const [globalAccount, setGlobalAccount] = useState<string | null | undefined>();
+  const [globalAccountResult, setGlobalAccountResult] = useState<string | null>(null);
+  const [globalAccountStatus, setGlobalAccountStatus] = useState<string | null>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -18,13 +19,18 @@ function Wallet() {
       setLoading(true);
       if(/^[a-zA-Z0-9\-_]{43}$/.test(addr.trim())){
         const walletStorage = await getGlobalStorageOfWallet(addr, arweave);
-        setGlobalAccount(walletStorage ? JSON.stringify(walletStorage, null, 2) : globalAccount);
-        setLoading(false);
+        setGlobalAccountStatus(walletStorage.status);
+        setGlobalAccountResult(walletStorage.status !== "error" ? JSON.stringify(walletStorage.result, null, 2) : walletStorage.result);
       }
-      else
-        setGlobalAccount("invalid wallet address");
+      
+      setLoading(false);
     })()
-  }, [addr, globalAccount]);
+
+    return () => {
+      setGlobalAccountStatus(null);
+      setGlobalAccountResult(null);
+    }
+  }, [addr]);
 
   const activateGlobalAccount = async () => {
     const globalstorage = new GlobalStorage("Global-Account", arweave);
@@ -34,22 +40,29 @@ function Wallet() {
 
   return(
     <>
-      <div>wallet: {addr}</div>
+      <h1>Global Account</h1>
+      <h2><pre>{addr}</pre></h2>
       {loading ? "loading" :
-      <>
-        <div>Global Account: {
-          globalAccount === null ? "This user haven't activated their Global Account yet." : 
-            globalAccount === undefined ? "This wallet has activated or reset their Global Account recently and the tx is not confirmed yet."
-          : <JsonS>{globalAccount}</JsonS>}
-        </div>
-        <CenteredS>
-          {addr === walletAddr && 
-            <ButtonS onClick={activateGlobalAccount}>
-              {globalAccount !== null ? "Reset your Global Account" : "Activate your Global Account"}
-            </ButtonS>
-          }
-        </CenteredS>
-      </>}
+        globalAccountStatus ?
+        <>
+          <div>
+            {globalAccountStatus === "error" && <>Status: [Error] - {globalAccountResult}</>}
+            {globalAccountStatus === "pending" && <>Status: [Warning] - This user's Global Account was recently reset and its last state has not been confirmed by the network yet.</>}
+          </div>
+          {globalAccountStatus !== "error" && <>
+            <div>Result: </div>
+            <JsonS>{globalAccountResult}</JsonS>
+          </>}
+          <CenteredS>
+            {addr === walletAddr && 
+              <ButtonS onClick={activateGlobalAccount}>
+                {globalAccountStatus === "error" ? "Activate your Global Account" : "Reset your Global Account"}
+              </ButtonS>
+            }
+          </CenteredS>
+        </>
+        : "Invalid wallet address"
+      }
     </>
   );
 }
